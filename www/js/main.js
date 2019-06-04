1773fa34c868fc0ac7bd417402717d4698d61f2c
Marcin <marstan1@onet.pl>
2019-06-04 21:36:46 +0200

    local storage saving - transaction

--- www/js/main.js
+++ www/js/main.js
@@ -1,19 +1,22 @@
-﻿(function () {
+﻿var lat = 0;
+var lng = 0;
+(function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Obsługa zdarzeń wstrzymywania i wznawiania działania oprogramowania Cordova
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
         // TODO: Załadowano oprogramowanie Cordova. Wykonaj tutaj wszystkie wymagane kroki inicjowania tego oprogramowania.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 4000, maximumAge: 10000, enableHighAccuracy: true });
     };
 
     function onPause() {
@@ -65,13 +68,13 @@ function includeHTML() {
 
 // funkcja obsługuje proces kliknięcia w menu wyszukiwania
 function expand() {
-  $(".search").toggleClass("close");
-  $(".input").toggleClass("square");
-  if ($('.search').hasClass('close')) {
-    $('input').focus();
-  } else {
-    $('input').blur();
-  }
+    $(".search").toggleClass("close");
+    $(".input").toggleClass("square");
+    if ($('.search').hasClass('close')) {
+        $('input').focus();
+    } else {
+        $('input').blur();
+    }
 }
 $('button').on('click', expand);
 
@@ -112,9 +115,6 @@ function displayShow(idElement, bool) {
 function getData() {
     $(".form_datetime").datetimepicker({ format: 'yyyy-mm-dd' });
 }
-// plik javaScript do definowania funkcji
-// metody objete w funkcjach jss 
-
 
 function logIn() {
     $(".back").click(function () {
@@ -145,8 +145,8 @@ function internetConnection() {
     }
 }
 
-function onDeviceReady() {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 5000, maximumAge: 10000, enableHighAccuracy: true });
+function GPSDevice() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 4000, maximumAge: 10000, enableHighAccuracy: true });
 }
 
 function onSuccess(pos) {
@@ -155,15 +155,14 @@ function onSuccess(pos) {
 }
 
 function onError(error) {
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-    return "brak danych";
+    //alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
 }
 
 // Oblicza dystans w linii prostej między dwoma punktami.
 // Na wejściu metoda otrzymuje dane lokalizacyjne dwóch punktów.
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371; 
-    var dLat = deg2rad(lat2 - lat1); 
+    var R = 6371;
+    var dLat = deg2rad(lat2 - lat1);
     var dLon = deg2rad(lon2 - lon1);
     var a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
@@ -194,49 +193,51 @@ function showGoogleMaps(lat, lon) {
 
 // Metoda wykonuje zapytanie na api z quantor.pl.
 function searchOffer() {
-   //onDeviceReady();
-   var e = document.getElementById("transaction");
-   var transactionQuantor = e.options[e.selectedIndex].value;
-   var f = document.getElementById("currency");
-   var currencyQuantor = f.options[f.selectedIndex].value;
-   if (this.internetConnection() === "true") {
-       let xmlHttp = new XMLHttpRequest();
-       xmlHttp.open("POST", "https://quantor.pl/api/cantormap", true); // false for synchronous request
-       xmlHttp.setRequestHeader("X-AUTH-TOKEN", "zuBtJ6gS7Vh7Wrcf");
-       xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-       xmlHttp.setRequestHeader("User-Agent", "PostmanRuntime/7.11.0");
-       xmlHttp.setRequestHeader("Accept", "*/*");
-       xmlHttp.setRequestHeader("Cache-Control", "no-cache");
-       xmlHttp.setRequestHeader("Postman-Token", "174e13cd-4edf-4598-9953-169b3f1f2eab,430aecde-2f62-4b3e-a8cf-024b7d64a687");
-       xmlHttp.setRequestHeader("Host", "quantor.pl");
-       xmlHttp.setRequestHeader("cookie", "device_view=full");
-       xmlHttp.setRequestHeader("accept-encoding", "gzip, deflate");
-       xmlHttp.setRequestHeader("content-length", "33");
-       xmlHttp.setRequestHeader("Connection", "keep-alive");
-       xmlHttp.timeout = 5000;
-   
-       xmlHttp.addEventListener('load', function () {
-           if (this.status === 200) {
-               getValue(JSON.parse(this.responseText), transactionQuantor);
-           }
-           else {
-               alert('Połączenie zakończyło się statusem ' + this.status + ' spróbuj ponownie za chwile');
-           }
-       });
-   
-       xmlHttp.addEventListener('error', function (e) {
-           alert('Wystąpił błąd połączenia');
-       });
-   
-       xmlHttp.addEventListener('timeout', function () {
-           alert('Upłynął czas zapytania, proszę spróbować ponownie za chwilę');
-       });
-   
-       xmlHttp.send("currency=" + currencyQuantor+"&transaction="+transactionQuantor);
-   }
-   else {
-       alert("Brak dostępu do internetu");
-   }
+    GPSDevice();
+    if (lat != 0 && lng != 0) {
+        var e = document.getElementById("transaction");
+        var transactionQuantor = e.options[e.selectedIndex].value;
+        var f = document.getElementById("currency");
+        var currencyQuantor = f.options[f.selectedIndex].value;
+        if (this.internetConnection() === "true") {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("POST", "https://quantor.pl/api/cantormap", true); // false for synchronous request
+            xmlHttp.setRequestHeader("X-AUTH-TOKEN", "zuBtJ6gS7Vh7Wrcf");
+            xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xmlHttp.setRequestHeader("User-Agent", "PostmanRuntime/7.11.0");
+            xmlHttp.setRequestHeader("Accept", "*/*");
+            xmlHttp.setRequestHeader("Cache-Control", "no-cache");
+            xmlHttp.setRequestHeader("Postman-Token", "174e13cd-4edf-4598-9953-169b3f1f2eab,430aecde-2f62-4b3e-a8cf-024b7d64a687");
+            xmlHttp.setRequestHeader("Host", "quantor.pl");
+            xmlHttp.setRequestHeader("cookie", "device_view=full");
+            xmlHttp.setRequestHeader("accept-encoding", "gzip, deflate");
+            xmlHttp.setRequestHeader("content-length", "33");
+            xmlHttp.setRequestHeader("Connection", "keep-alive");
+            xmlHttp.timeout = 5000;
+
+            xmlHttp.addEventListener('load', function () {
+                if (this.status === 200) {
+                    getValue(JSON.parse(this.responseText), transactionQuantor);
+                }
+                else {
+                    alert('Połączenie zakończyło się statusem ' + this.status + ' spróbuj ponownie za chwile');
+                }
+            });
+
+            xmlHttp.addEventListener('error', function (e) {
+                alert('Niestety coś poszło nie tak :/');
+            });
+
+            xmlHttp.addEventListener('timeout', function () {
+                alert('Coś tłoczno na łączu internetowym :/ spróbuj ponownie za chwilę :)');
+            });
+
+            xmlHttp.send("currency=" + currencyQuantor + "&transaction=" + transactionQuantor);
+        }
+        else {
+            alert("No wiesz..., internet jest mi potrzebny :) ");
+        }
+    }
 }
 
 // Funkcja wyciąga potrzebne dane z pliku JSON i następnie eksportuje je do swojego pliku JSON do późniejszych operacji.
@@ -267,8 +268,6 @@ function getValue(dane, transactionQuantor) {
                         }
                     }
                     else if (counter == 3) {
-                        //lat = 50.2585222;
-                        //lng = 20.2585241;
                         var distance = getDistanceFromLatLonInKm(lat, lng, dane.rates[step].lat, dane.rates[step].lng);
                         myObject.distance = distance;
                     }
@@ -333,7 +332,7 @@ function sortJSON(dane, transactionQuantor) {
                 }
                 else {
                     check += 0;
-                } 
+                }
             }
             else {
                 if (parseFloat(dane[i].value) > parseFloat(dane[i - 1].value)) {
@@ -367,7 +366,7 @@ function sortJSON(dane, transactionQuantor) {
                 else {
                     check += 0;
                 }
-            } 
+            }
         }
     } while (check > 0);
     var objectJSON = JSON.stringify(dane);
@@ -392,7 +391,7 @@ function showDataJSON(dane) {
                         this.createElementDivWithGPS(div, "streetText", dane[step].street, dane[step].lat, dane[step].lng);
                     }
                     else if (counter == 2) {
-                            this.createElementDiv(div, "courseText", ("KURS: " + dane[step].value));
+                        this.createElementDiv(div, "courseText", ("KURS: " + dane[step].value));
                     }
                     else if (counter == 3) {
                         var distance = getDistanceFromLatLonInKm(lat, lng, dane[step].lat, dane[step].lng);
@@ -413,7 +412,7 @@ function showDataJSON(dane) {
 
 // Funkcja tworzy element Div z odpowienimi danymi.
 // Na wejściu funkcja otrzymuje główny uchwyt do elementu, nazwę klasy oraz wartość do wstawienie.
-function createElementDiv(mainDiv,nameClass,value) {
+function createElementDiv(mainDiv, nameClass, value) {
     var div = document.createElement('div');
     div.setAttribute("class", nameClass);
     div.appendChild(document.createTextNode(value));
@@ -431,4 +430,65 @@ function createElementDivWithGPS(mainDiv, nameClass, value, lat, lng) {
     img.src = "images/gps.png";
     div.appendChild(img);
     mainDiv.appendChild(div);
+}
+
+//------------------------------------------ skrypty do dodawania transakcji-----
+
+// Funkcja pobiera poszczególne dane z formularza dodawania transakcji.
+// Jeśli wszystko przebiegnie prawidłowo (pola nie są puste) metoda dodaje dane do pamięci urządzenia w postaci struktury JSON.
+function addTransaction() {
+    var tran = document.getElementById("transactionEnd");
+    var transactionQuantor = tran.options[tran.selectedIndex].value;
+    var curr = document.getElementById("currencyEnd");
+    var currencyQuantor = curr.options[curr.selectedIndex].value;
+    var cant = document.getElementById("cantorEnd");
+    var cantor = cant.options[cant.selectedIndex].value;
+    var howMuch = document.getElementById("howMuch").value;
+    var unitRate = document.getElementById("unitRate").value;
+    var date = document.getElementById("getDate").value;
+    if (howMuch !== "" && unitRate !== "" && date !== "") {
+        var object = new Object();
+        var result = new Array();
+        object.tranzakcja = transactionQuantor;
+        object.waluta = currencyQuantor;
+        object.kantor = cantor;
+        object.ilosc = howMuch;
+        object.kurs = unitRate;
+        object.wartosc = unitRate * howMuch;
+        object.data = date;
+        result[0] = object;
+        text = localStorage.getItem("userHistory");
+        if (text == null) { // warunek sprawdzający czy taki plik istnieje
+            objectJSON = JSON.stringify(result);
+        }
+        else {
+            obj = JSON.parse(text);
+            obj.push(result[0]);
+            objectJSON = JSON.stringify(obj);
+        }
+        localStorage.setItem("userHistory", objectJSON);
+        delete object;
+        delete result;
+        alert("No i masz to w swojej historii :)");
+    }
+    else {
+        alert("Wszystkie pola są ważne :)");
+    }
+}
+
+function showTest() {
+    text = localStorage.getItem("userHistory");
+    obj = JSON.parse(text);
+    document.getElementById('historyID').innerHTML = "";
+    var body = document.getElementsByTagName('body')[0];
+    for (var i = 0; i < obj.length; i++) {
+        var div = document.createElement('div');
+        div.setAttribute("class", "historyBox");
+        this.createElementDiv(div, "cantorText", obj[i].data);
+        this.createElementDiv(div, "testHistory", obj[i].tranzakcja + ": " + obj[i].ilosc + " " + obj[i].waluta);
+        this.createElementDiv(div, "testHistory", "Po kursie: " + obj[i].kurs);
+        this.createElementDiv(div, "testHistory", "Na kwotę: " + obj[i].wartosc + " zł");
+        this.createElementDiv(div, "testHistory", "W kantorze: " + obj[i].kantor);
+        body.appendChild(div);
+    }
 }
