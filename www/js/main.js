d09f15f9f37f4b57f11defc3ab8f5a7a01309264
Malgorzata <nizio.malgorzata94@gmail.com>
2019-06-04 21:58:09 +0200

    Dodano logowanie i rejestracje przez mail Firebase)

--- www/js/main.js
+++ www/js/main.js
@@ -116,7 +116,7 @@ function getData() {
 // metody objete w funkcjach jss 
 
 
-function logIn() {
+/*function logIn() {
     $(".back").click(function () {
         $(".signUp").addClass("active-sx");
         $(".signIn").addClass("inactive-dx");
@@ -132,7 +132,7 @@ function signIn() {
         $(".signUp").removeClass("active-sx");
         $(".signIn").removeClass("inactive-dx");
     });
-}
+}*/
 
 // Funkcja sprawdza połączenie z internetem
 function internetConnection() {
@@ -432,3 +432,87 @@ function createElementDivWithGPS(mainDiv, nameClass, value, lat, lng) {
     div.appendChild(img);
     mainDiv.appendChild(div);
 }
+
+//Logowanie przez mail przy wykorzystaniu Firebase
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+  
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(user != null){
+  
+        var email_id = user.email;
+        var email_verified = user.emailVerified;
+
+        if(email_verified) {
+            document.getElementById("verify_btn").style.display = "none";
+        }
+        else {
+            document.getElementById("verify_btn").style.display = "block";
+        }
+
+        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id + "<br/> Verified: " + email_verified;
+  
+      }
+  
+    } else {
+      // No user is signed in.
+  
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+  
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+  
+  function logout(){
+    firebase.auth().signOut();
+  };
+
+  function create_account() {
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    
+        window.alert("Error : " + errorMessage);
+})};
+
+  function send_verification() {
+
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification.then(function() {
+        //Email send
+        window.alert("Verification sent");
+    }).catch(function(error) {
+        //error happenned
+        window.alert("Error: " + error.message);
+    })
+  }
+  
